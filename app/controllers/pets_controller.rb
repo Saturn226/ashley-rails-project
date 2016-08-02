1997c69d7775f45c6629fdfd2cf49eaa4d039ec7
Saturn226 <serenehopes@gmail.com>
2016-08-02 15:35:11 -0400

    Added show and index actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,34 @@
 class PetsController < ApplicationController
   def index
+    if params[:user_id]
+      @pets = User.find(params[:user_id]).pets
+    else
+      @pets = Pet.all
+    end
+  end
+
+  def show
+    if params[:user_id]
+      @pet = Pet.find(params[:id])
+    else
+      @pet = Pet.find(params[:id])
+    end
   end
 
   def new
-    pet = Pet.new
+    @pet = Pet.new(user_id: params[:user_id])
+  end
+
+  def create
+      pet = current_user.pets.build(pet_params)
+      if pet.save
+        redirect user_path
+      else
+        redirect new_user_pet_path
+      end
+  end
+
+  def pet_params
+    params.require(pet).permit(:name, :bio, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ end
   resources :users do
     resources :pets
   end
+
+  resources :pets
   #root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
