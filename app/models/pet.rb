04f89cdfa9bdede21c4624c7d26d823338e60f04
Saturn226 <serenehopes@gmail.com>
2016-08-15 14:40:40 -0400

    Fixed nested form. Works now

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,18 +2,17 @@ class Pet < ActiveRecord::Base
   belongs_to :user
   has_many :pet_breeds
   has_many :breeds, through: :pet_breeds
-  accepts_nested_attributes_for :breeds, reject_if: lambda { |attributes| attributes[:name].blank? }
+  # accepts_nested_attributes_for :breeds, reject_if: lambda { |attributes| attributes[:name].blank? }
 
   validates :name, presence: true
  
   def breeds_attributes=(breeds_attributes)
 
     breeds_attributes.values.each do |attribute|
-      #binding.pry
       if attribute[:name].present?  
         breed = Breed.find_or_create_by(attribute)
-        if !self.breeds.include?(name)
-          self.breeds.build(:name => name)
+        if !self.breeds.include?(attribute[:name])
+          self.pet_breeds.build(breed_id: breed.id)
         end
       end
     end
