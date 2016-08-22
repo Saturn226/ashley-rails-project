bde3dc51d79424a3a4c688f2a8856a9291466908
Saturn226 <serenehopes@gmail.com>
2016-08-22 00:58:14 -0400

    Added first and last name to signup and edit

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,5 +1,6 @@
 class Breed < ActiveRecord::Base
   has_many :pet_breeds
   has_many :pets, through: :pet_breeds, dependent: :destroy
+  has_many :users, through: :pets
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #devise_for :users
 
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks",  registrations: 'registrations'  }
 
 
 
