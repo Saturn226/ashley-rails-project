30a7cf53194a0cc04f7064ef737c35b7fbc53f74
Saturn226 <serenehopes@gmail.com>
2016-09-18 21:25:21 -0400

    removed prevent default from script file

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,10 +1,9 @@
 $(function(){
   $('#adoptButton').on('click', function(e){
     //show adopt a pet page without refresh
-    e.preventDefault();
+   // e.preventDefault();
     $.get("/pets", function(data){
-      $('#users-show').remove();
-      $('#pets-index').append(data)
+     
     })
   });
 });
